28a816eef1ba6a3e7d44ea823df972feefa46dd4
User <rabbiprimon00000@gmail.com>
2024-03-26 06:48:14 +0600

    flutter-module6-cls2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//MaterialApp| StatelessWidget| Scaffold| Text| Center|
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +6,6 @@ void main() {
       MaterialApp(
           title: 'Hello Primon',
           home: Home()
-
       )
   );
 }
@@ -15,11 +15,17 @@ class Home extends StatelessWidget {
   //whatever you write will show in screen
   return Scaffold(
   body: Center(
-  child: Text('Happy learning', style: TextStyle(
-    fontSize: 24,
-  ),),  
+  //child: Text('Happy learning', style: TextStyle(
+    //fontSize: 24,
+    /*child: Image.asset('Images/RangeRover.jpg',
+      width: 300,
+      height: 300,
+      fit: BoxFit.fitWidth,),*/
+ child: Image.network('https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+   width: 300,
+   height: 400,
+   fit: BoxFit.scaleDown,),
+  ),
   );
-);
   }
-
       }
