b31a23d77f59a54dd982c7dc48df7ba1520ba05c
User <rabbiprimon00000@gmail.com>
2024-04-17 19:19:40 +0600

    flutter-module6-cls 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+//Scaffold| Row| Column| Dialog|
+/*import 'dart:js_interop';
+
+import 'package:flutter/cupertino.dart';*/
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,16 +14,62 @@ void main() {
       )
   );
 }
+/* Column => Vertically => mainAxis
+   Row => Horizontally => crossAxis
+*/
 class Home extends StatelessWidget {
 @override
   Widget build (BuildContext ){
   //whatever you write will show in screen
   return Scaffold(
+    backgroundColor: Colors.tealAccent,
+  appBar: AppBar(
+    backgroundColor: Colors.white70,
+    title: Text('Home Screen',style: TextStyle(
+        color: Colors.black ),
+    ),
+    leading: Icon(Icons.home_filled, color: Colors.black,),
+    actions: [
+      IconButton(onPressed: () {}, icon: Icon(Icons.add))
+    ],
+  ),
   body: Center(
-  child: Text('Happy learning', style: TextStyle(
-    fontSize: 24,
-  ),),  
-  );
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Text('Hello'),
+      Text('primon bro'),
+      Text('from'),
+      Text('Ostad'),
+
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text('Good morning '),
+          Text('from Primon '),
+
+          Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.date_range),
+                  Text('date'),
+                ],
+              ),
+              
+              Text('29-09-2000'),
+            ],
+          )
+        ],
+      )
+    ],
+  )
+  )
 );
   }
 
