cde4294e4c848afb5872eb15b08f2bb8c20b0df9
User <rabbiprimon00000@gmail.com>
2024-05-01 00:10:25 +0600

    flutter-module7-cls1

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,67 @@
-//Button ( Elevated button, TextButton IconButton,GestureDetector, InkWell,)| TextFeild | Container|
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp
-    (title: 'Hello Primon',
-      home: Home()
-  )
-  );
+  runApp(MyApp());
 }
 
-/* Column => Vertically => mainAxis
-   Row => Horizontally => crossAxis
-*/
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext) {
-    //whatever you write will show in screen
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'new app',
+      home: ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.tealAccent,
-        appBar: AppBar(
-          backgroundColor: Colors.white70,
-          title: Text(
-            'Home Screen',
-            style: TextStyle(color: Colors.black),
-          ),
-          leading: Icon(
-            Icons.home_filled,
-            color: Colors.black,
-          ),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)
-                    )
-                ),
-                onPressed: () {
-                  print("Pressed");
-                },
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/RangeRover.jpg'),
+                  ),
+                  SizedBox(width: 16.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Range Rover',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'matildabrown@mail.com',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
                 child: Text(
-                  "Press here",
-                ))
+                  'The Land Rover Range Rover, generally shortened to Range Rover, is a 4x4 luxury SUV produced by Land Rover, a marque and sub-brand of Jaguar Land Rover. The Range Rover line was launched in 1970 by British Leyland and is now in its fifth generation.',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+            ),
           ],
-        )
-        )
+        ),
+      ),
     );
   }
 }
